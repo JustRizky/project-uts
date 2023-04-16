3873a0b95210b4148c241b13eb88dfbb6be5cbc1
RanggaAdisyah <116618151+RanggaAdisyah@users.noreply.github.com>
2023-04-16 10:21:14 +0700

    Add files via upload

--- UTS struktur data.go
+++ UTS struktur data.go
@@ -25,9 +25,6 @@ type Customer struct {
 	Next *Customer
 }
 
-type LinkedList struct {
-	Head *Menu
-}
 
 type LinkedListKSR struct {
 	head *Kasir
@@ -97,24 +94,78 @@ func (list *LinkedListMenu) InsertMenu(menu *Menu) {
 
 }
 
+//UPDATE
+
+
+func (list *LinkedListCS) UpdatenamaCS(oldCS *Customer, newCS string) {
+	if list.head == nil {
+		fmt.Println("TIDAK ADA BARANG")
+	}
+
+	curr := list.head
+		for curr != nil {
+			if curr == oldCS {
+				curr.Nama = newCS
+				return
+			}
+			curr=curr.Next
+		}
+}
+
+func(list *LinkedListKSR) UpdatenamaKSR(oldKSR *Kasir, newKSR string) {
+	if list.head == nil {
+		fmt.Println("TIDAK ADA BARANG")
+	}
+
+	curr := list.head
+		for curr != nil {
+			if curr == oldKSR {
+				curr.Nama = newKSR
+				return
+			}
+			curr=curr.Next
+		}
+}
+
+func(list *LinkedListMenu) UpdatenamaMENU(oldMENU *Menu, newMAKAN string, newMINUM string) {
+	if list.head == nil {
+		fmt.Println("TIDAK ADA BARANG")
+	}
+
+	curr := list.head
+		for curr != nil {
+			if curr == oldMENU {
+				curr.Makanan = newMAKAN
+				curr.Minuman = newMINUM
+				return
+			}
+			curr=curr.Next
+		}
+}
+
+
+//DELETE
+
 func (list *LinkedListKSR) DeleteKSR(ID int) {
+	// jika node kosong
 	if list.head == nil {
 		fmt.Println("Kasir tidak ditemukan")
 		return
 	}
 
+	// jika node 
 	if list.head.Nik == ID {
 		list.head = list.head.Next
 		return
 	}
 
-	prevKasir := list.head
-	for prevKasir.Next != nil {
-		if prevKasir.Next.Nik == ID {
-			prevKasir.Next = prevKasir.Next.Next
+	currKasir := list.head
+	for currKasir.Next != nil {
+		if currKasir.Next.Nik == ID {
+			currKasir.Next = currKasir.Next.Next
 			return
 		}
-		prevKasir = prevKasir.Next
+		currKasir = currKasir.Next
 	}
 
 	fmt.Println("Kasir tidak ditemukan")
@@ -131,13 +182,13 @@ func (list *LinkedListCS) DeleteCS(ID int) {
 		return
 	}
 
-	prevCustomer := list.head
-	for prevCustomer.Next != nil {
-		if prevCustomer.Next.Np == ID {
-			prevCustomer.Next = prevCustomer.Next.Next
+	currCustomer := list.head
+	for currCustomer.Next != nil {
+		if currCustomer.Next.Np == ID {
+			currCustomer.Next = currCustomer.Next.Next
 			return
 		}
-		prevCustomer = prevCustomer.Next
+		currCustomer = currCustomer.Next
 	}
 
 	fmt.Println("Customer tidak ditemukan")
@@ -154,19 +205,19 @@ func (list *LinkedListMenu) DeleteMenu(ID int) {
 		return
 	}
 
-	prevMenu := list.head
-	for prevMenu.Next != nil {
-		if prevMenu.Next.ID == ID {
-			prevMenu.Next = prevMenu.Next.Next
+	currMenu := list.head
+	for currMenu.Next != nil {
+		if currMenu.Next.ID == ID {
+			currMenu.Next = currMenu.Next.Next
 			return
 		}
-		prevMenu = prevMenu.Next
+		currMenu = currMenu.Next
 	}
 
 	fmt.Println("Menu tidak ditemukan")
 }
 
-
+//VIEWALL
 
 func (list *LinkedListCS) ViewAllCustomer() {
 	if list.head == nil {
@@ -177,7 +228,7 @@ func (list *LinkedListCS) ViewAllCustomer() {
 		fmt.Println("Customer :")
 		for tempRestoran != nil {
 
-			fmt.Println("___________________________")
+			fmt.Println("_________")
 			fmt.Println("ID   : ", tempRestoran.Np)
 			fmt.Println("Nama : ", tempRestoran.Nama)
 			tempRestoran = tempRestoran.Next
@@ -194,7 +245,7 @@ func (list *LinkedListKSR) ViewAllKasir() {
 		fmt.Println("Kasir :")
 		for tempRestoran != nil {
 
-			fmt.Println("___________________________")
+			fmt.Println("_________")
 			fmt.Println("ID   : ", tempRestoran.Nik)
 			fmt.Println("Nama : ", tempRestoran.Nama)
 			tempRestoran = tempRestoran.Next
@@ -211,7 +262,7 @@ func (list *LinkedListMenu) ViewAllMenu() {
 		fmt.Println("Makanan :")
 		for tempRestoran != nil {
 
-			fmt.Println("___________________________")
+			fmt.Println("_________")
 			fmt.Println("ID   : ", tempRestoran.ID)
 			fmt.Println("Makanan : ", tempRestoran.Makanan)
 			fmt.Println("Minuman : ", tempRestoran.Minuman)
@@ -255,24 +306,37 @@ func main() {
 	}
 
 	menu2 := &Menu{
-        ID:      02,
-        Makanan: "Tahu Tek",
-        Minuman: "Es Kepal",
-	    }
+		ID:      02,
+		Makanan: "Tahu Tek",
+		Minuman: "Es Kepal",
+	}
 
 	list2.InsertCS(customer1)
-	list2.InsertCS(customer2)	
+	list2.InsertCS(customer2)
 	list3.InsertKSR(kasir1)
 	list3.InsertKSR(kasir2)
 	list3.InsertKSR(kasir3)
 	list4.InsertMenu(menu1)
 	list4.InsertMenu(menu2)
 
+	list3.ViewAllKasir()
+	list2.ViewAllCustomer()
+	list4.ViewAllMenu()
+
+	fmt.Println("\n========= SESUDAH DELETE ============")
 
 	list3.DeleteKSR(01)
 	list4.DeleteMenu(01)
 	list2.DeleteCS(99)
 
+	list3.ViewAllKasir()
+	list2.ViewAllCustomer()
+	list4.ViewAllMenu()
+
+	fmt.Println("\n========= SESUDAH UPDATE ============")
+	list3.UpdatenamaKSR(kasir2 , "RIZKI IMOET")
+	list4.UpdatenamaMENU(menu2, "SEGO JAGUNG" , "ES DEGAN")
+	list2.UpdatenamaCS(customer2 , "RANGGA GANTENG")
 	list3.ViewAllKasir()
 	list2.ViewAllCustomer()
 	list4.ViewAllMenu()
