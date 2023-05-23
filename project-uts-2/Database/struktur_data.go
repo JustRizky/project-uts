d7f0640f83a220977e06712a96dbe6860103dce6
M. Rizky Kurniawan Lasim <111512177+JustRizky@users.noreply.github.com>
2023-05-24 00:48:38 +0700

    Delete project-uts-2 directory

--- project-uts-2/Database/struktur_data.go
+++
@@ -1,7 +0,0 @@
-package Database
-
-import "project-uts-2/entity"
-
-var DBCustomer entity.LinkedListCustomer
-var DBKasir entity.LinkedListKasir
-var DBMenu entity.LinkedListMenu
--- project-uts-2/go.mod
+++
@@ -1,3 +0,0 @@
-module project-UTS/project-uts-2
-
-go 1.20
